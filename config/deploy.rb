4293a8d2cdfd518d0458751e79d14725d43e033a
kmucheru <kevinmucheruk@gmail.com>
2017-10-27 18:05:07 +0300

    setdeploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/webelfs"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
