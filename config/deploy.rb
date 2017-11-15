cfdf3e2b4d7b9241e1b5e1d709adca0c11102f43
kmucheru <kevinmucheruk@gmail.com>
2017-11-15 10:10:29 +0300

    dbmerge

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@
 set :application, "webelfs"
 set :repo_url, "https://github.com/kmucheru/SiteNuts.git"
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'db/production.sqlite3')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default branch is :master
@@ -37,60 +37,3 @@ set :deploy_to, "/var/www/webelfs"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-Capistrano::Configuration.instance.load do
-
-  namespace :deploy do
-
-    namespace :db do
-
-      desc <<-DESC
-        Creates the database.yml configuration file in shared path.
-        By default, this task uses a template unless a template \
-        called database.yml.erb is found either is :template_dir \
-        or /config/deploy folders. The default template matches \
-        the template for config/database.yml file shipped with Rails.
-        When this recipe is loaded, db:setup is automatically configured \
-        to be invoked after deploy:setup. You can skip this task setting \
-        the variable :skip_db_setup to true. This is especially useful \
-        if you are using this recipe in combination with \
-        capistrano-ext/multistaging to avoid multiple db:setup calls \
-        when running deploy:setup for all stages one by one.
-      DESC
-      task :setup, :except => { :no_release => true } do
-
-        default_template = <<-EOF
-        base: &base
-          adapter: sqlite3
-          timeout: 5000
-        
-
-        production:
-          database: #{shared_path}/db/production.sqlite3
-          <<: *base
-        EOF
-
-        location = fetch(:template_dir, "config/deploy") + '/database.yml.erb'
-        template = File.file?(location) ? File.read(location) : default_template
-
-        config = ERB.new(template)
-
-        run "mkdir -p #{shared_path}/db"
-        run "mkdir -p #{shared_path}/config"
-        put config.result(binding), "#{shared_path}/config/database.yml"
-      end
-
-      desc <<-DESC
-        [internal] Updates the symlink for database.yml file to the just deployed release.
-      DESC
-      task :symlink, :except => { :no_release => true } do
-        run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-      end
-
-    end
-
-    after "deploy:setup",           "deploy:db:setup"   unless fetch(:skip_db_setup, false)
-    after "deploy:finalize_update", "deploy:db:symlink"
-
-  end
-
-end
